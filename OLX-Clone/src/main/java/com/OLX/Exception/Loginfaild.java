71e9c73ca4d423a62420f05f25189bbe5721f0cd
91801 <91801@Sashwat>
2025-08-19 22:28:12 +0530

    add Login failed exception

---
+++ OLX-Clone/src/main/java/com/OLX/Exception/Loginfaild.java
@@ -0,0 +1,16 @@
+package com.OLX.Exception;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class Loginfaild extends Exception {
+	
+	public Loginfaild(String messge)
+	{
+		super(messge);
+		
+	}
+	
+	
+
+}
